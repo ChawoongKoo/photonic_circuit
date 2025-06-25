82c8c6c878efc69c51a91dd3d837af85de4d5716
chawoong <chawoong@umich.edu>
2025-06-25 14:17:49 -0400

    added simple mzi analysis

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()  # Loads .env into os.environ automatically
 
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from models import coupler, waveguide, mzi
+
+w1 = jnp.linspace(1.51, 1.59, 1000)
+s_matrix = mzi(w1=w1, top={"length": 25.}, bot={"length": 15.})
+
+plt.plot(w1*1e3, abs(s_matrix["in0", "out0"])**2)
+plt.ylim(-.05, 1.05)
+plt.xlabel("nm")
+plt.ylabel("T")
+plt.show()
