df9b13f2f7283fb460877f3244b5b75f9579bf3e
chawoong <chawoong@umich.edu>
2025-06-25 11:31:05 -0400

    added mzi component model

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ import jax.numpy as jnp
 
 # definition of a simple coupler
 # returns transmission/reflection of input/output port combinations 
-def coupler(coupling=0.5):
+def coupler(coupling=0.5) -> sax.SDict:
     kappa = coupling**0.5
     tau = (1-coupling)**0.5
     sdict = sax.reciprocal({
@@ -23,9 +23,9 @@ def coupler(coupling=0.5):
 
 # print(coupler(coupling=.3))
 
-#definition of a simple waveguide
-#returns the transmission through the waveguide
-def waveguide(w1=1.55, w10=1.55, neff=2.34, ng=3.4, length=10, loss=0.):
+# definition of a simple waveguide
+# returns the transmission through the waveguide
+def waveguide(w1=1.55, w10=1.55, neff=2.34, ng=3.4, length=10, loss=0.) -> sax.SDict:
     dw1 = w1 - w10
     dneff_dw1 = (ng - neff) / w10
     neff = neff - (dw1 * dneff_dw1)
@@ -38,3 +38,34 @@ def waveguide(w1=1.55, w10=1.55, neff=2.34, ng=3.4, length=10, loss=0.):
 
 # print(waveguide(length=100))
 
+# Definition of a Mach Zehnder Interferometer
+# It is a composite component, basically being
+# a circuit made up of couplers and waveguides
+mzi, info = sax.circuit(
+    netlist={
+        "instances": {
+            "left": "coupler",
+            "top": "waveguide",
+            "bot": "waveguide",
+            "right": "coupler"
+        },
+        "connections": {
+            "left,out0": "bot,in0",
+            "bot,out0": "right,in0",
+            "left,out1": "top,in0",
+            "top,out0": "right,in1"
+        },
+        "ports": {
+            "in0": "left,in0",
+            "in1": "left,in1",
+            "out0": "right,out0",
+            "out1": "right,out1"
+        }
+    },
+    models = {
+        "coupler": coupler,
+        "waveguide": waveguide
+    }
+)
+
+# print(type(mzi))
