229875153ac80a1b25abfe2165c2368082025af3
chawoong <chawoong@umich.edu>
2025-06-26 13:13:46 -0400

    added model factory for a chain of MZI's

--- circuit_simulation/models.py
+++ circuit_simulation/models.py
@@ -66,3 +66,55 @@ mzi, info = sax.circuit(
         "waveguide": waveguide
     }
 )
+
+# Directional coupler definition 
+# for use in a chain of MZI's
+# Essentially the same as the coupler component
+dc_with_arms, info = sax.circuit(
+    netlist={
+        "instances": {
+            "left": "coupler",
+            "bot": "waveguide",
+            "top": "waveguide"
+        },
+        "connections": {
+            "left,out0": "bot,in0",
+            "left,out1": "top,in0"
+        },
+        "ports": {
+            "in0": "left,in0",
+            "in1": "left,in1",
+            "out0": "bot,out0",
+            "out1": "top,out0"
+        }
+    },
+    models={
+        "coupler": coupler,
+        "waveguide": waveguide
+    }
+)
+
+# Function/model factory that returns a chain of 
+# MZI's implemented by taking the output of a pervious directional coupler
+# and linking it with the input of the next coupler
+def mzi_chain(num_mzis=1) -> sax.Model:
+    chain, _ = sax.circuit(
+        netlist= {
+            "instances": {f"dc{i}": "dc_with_arms" for i in range(num_mzis+1)},
+            "connections": {
+                **{f"dc{i},out0": f"dc{i+1},in0" for i in range(num_mzis)},
+                **{f"dc{i},out1": f"dc{i+1},in1" for i in range(num_mzis)}
+            },
+            "ports": {
+                "in0": "dc0,in0",
+                "in1": "dc0,in1",
+                "out0": f"dc{num_mzis},out0",
+                "out1": f"dc{num_mzis},out1"
+            }
+        },
+        models={"dc_with_arms": dc_with_arms},
+        backend="klu",
+        return_type="SDense"
+    )
+    return sax.sdict(chain)
+
